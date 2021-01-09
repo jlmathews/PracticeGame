46235529b0ec4d3525bd1a4c1039fa6200db8233
jlmathews <jlmathews8@gmail.com>
2021-01-09 10:02:32 -0800

    add storage adapter for redis (#3)

---
+++ src/utilities/storage/include/storage_interface.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "spdlog/spdlog.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace PGame
+{
+    template <typename Adapter>
+    class IStorage
+    {
+        public:
+            IStorage(std::string tableName);
+
+            std::string GetValue(std::string key);
+            bool SetValue(std::string key, std::string value);
+
+            long long Increment(std::string key);
+            long long Decrement(std::string key);
+
+            bool SetAdd(std::string key, std::string value);
+            bool SetRemove(std::string key, std::string value);
+            void SetMembers(std::string key, std::vector<std::string> &values);
+
+            void HashGet(std::string key, std::initializer_list<std::string> keys, std::vector<std::string> &values);
+            void HashGet(std::string key, std::string keys, std::string &values);
+            void HashSet(std::string key, std::initializer_list<std::pair<std::string, std::string>> keyValue);
+
+            bool DeleteAll(std::string pattern);
+        private:
+            std::unique_ptr<Adapter> adapter;
+            std::string tableName;
+    };
+
+    template <typename Adapter>
+    IStorage<Adapter>::IStorage(std::string tableName): tableName(tableName)
+    {
+        spdlog::info("Create IStorage");
+        adapter = std::make_unique<Adapter>();
+    }
+
+    template <typename Adapter>
+    std::string IStorage<Adapter>::GetValue(std::string key)
+    {
+        return adapter->GetValue(tableName, key);
+    }
+
+    template <typename Adapter>
+    bool IStorage<Adapter>::SetValue(std::string key, std::string value)
+    {
+        return adapter->SetValue(tableName, key, value);
+    }
+
+    template <typename Adapter>
+    long long IStorage<Adapter>::Increment(std::string key)
+    {
+        return adapter->Increment(tableName, key);
+    }
+
+    template <typename Adapter>
+    long long IStorage<Adapter>::Decrement(std::string key)
+    {
+        return adapter->Decrement(tableName, key);
+    }
+
+    template <typename Adapter>
+    bool IStorage<Adapter>::SetAdd(std::string key, std::string value)
+    {
+        return adapter->SetAdd(tableName, key, value);
+    }
+
+    template <typename Adapter>
+    bool IStorage<Adapter>::SetRemove(std::string key, std::string value)
+    {
+        return adapter->SetRemove(tableName, key, value);
+    }
+
+    template <typename Adapter>
+    void IStorage<Adapter>::SetMembers(std::string key, std::vector<std::string> &values)
+    {
+        return adapter->SetMembers(tableName, key, values);
+    }
+
+    template <typename Adapter>
+    void IStorage<Adapter>::HashGet(std::string key, std::initializer_list<std::string> keys, std::vector<std::string> &values)
+    {
+        adapter->HashGet(tableName, key, keys, values);
+    }
+
+    template <typename Adapter>
+    void IStorage<Adapter>::HashGet(std::string key, std::string keys, std::string &values)
+    {
+        std::vector<std::string> tempValues;
+        HashGet(key, {keys}, tempValues);
+        if(tempValues.size() > 0)
+        {
+            values = tempValues[0];
+        }
+    }
+
+    template <typename Adapter>
+    void IStorage<Adapter>::HashSet(std::string key, std::initializer_list<std::pair<std::string, std::string>> keyValue)
+    {
+        adapter->HashSet(tableName, key, keyValue);
+    }
+
+    template <typename Adapter>
+    bool IStorage<Adapter>::DeleteAll(std::string pattern)
+    {
+        adapter->DeleteAll(pattern);
+    }
+}
---
+++ src/utilities/storage/redis_adapter.cpp
@@ -0,0 +1,118 @@
+#include "redis_adapter.h"
+
+#include "spdlog/spdlog.h"
+
+namespace PGame
+{
+    RedisAdapter::RedisAdapter()
+    {
+        spdlog::info("{}", __func__);
+        redis = std::make_unique<Redis>("tcp://127.0.0.1:6379");
+    }
+
+    std::string RedisAdapter::GetValue(std::string table, std::string key)
+    {
+        auto value = redis->get(table + ":" + key);
+        std::string returnValue = "";
+
+        if(value)
+        {
+            spdlog::info("Key {}, Value: {}", key, value.value());
+            returnValue = value.value();
+        }
+        else
+        {
+            spdlog::warn("Could not find key {}.", key);
+        }
+
+        return returnValue;
+    }
+
+    bool RedisAdapter::SetValue(std::string table, std::string key, std::string value)
+    {
+        spdlog::info("{}: Table {}, Key {}, Value: {}", __func__, table, key, value);
+        return redis->set(table + ":" + key, value);
+    }
+
+    long long RedisAdapter::Increment(std::string table, std::string key)
+    {
+        spdlog::info("{}: Key {}", __func__, key);
+        redis->incr(table + ":" + key);
+    }
+
+    long long RedisAdapter::Decrement(std::string table, std::string key)
+    {
+        spdlog::info("{}: Key {}", __func__, key);
+        redis->decr(table + ":" + key);
+    }
+
+    bool RedisAdapter::SetAdd(std::string table, std::string key, std::string value)
+    {
+        spdlog::info("{}: Key {}, Value: {}", __func__, key, value);
+
+        if(redis->sadd(table + ":" + key, {value}) == 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool RedisAdapter::SetRemove(std::string table, std::string key, std::string value)
+    {
+        spdlog::info("{}: Key {}, Value: {}", __func__, key, value);
+
+        if(redis->srem(table + ":" + key, value) == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void RedisAdapter::SetMembers(std::string table, std::string key, std::vector<std::string> &values)
+    {
+        spdlog::info("{}: Key {}", __func__, key);
+        redis->smembers(table + ":" + key, std::back_inserter(values));
+    }
+
+    void RedisAdapter::HashGet(std::string table, std::string key, std::initializer_list<std::string> keys, std::vector<std::string> &values)
+    {
+        spdlog::info("{}: Key {}", __func__, key);
+        std::vector<OptionalString> tempValues;
+        redis->hmget(table + ":" + key, keys, std::back_inserter(tempValues));
+        for(auto tempValue: tempValues)
+        {
+            values.push_back(tempValue.value());
+        }
+    }
+
+    void RedisAdapter::HashSet(std::string table, std::string key, std::initializer_list<std::pair<std::string, std::string>> keyValue)
+    {
+        spdlog::info("{}: Key {}", __func__, key);
+        redis->hmset(table + ":" + key, keyValue);
+    }
+
+    bool RedisAdapter::DeleteAll(std::string pattern)
+    {
+        spdlog::info("{}: Pattern {}", __func__, pattern);
+        std::vector<OptionalString> tempKeys;
+        redis->keys(pattern, std::back_inserter(tempKeys));
+
+        if(tempKeys.size() == 0)
+        {
+            spdlog::warn("Pattern {} not found", pattern);
+            return false;
+        }
+
+        std::vector<std::string> delKeys;
+        for(auto key: tempKeys)
+        {
+            spdlog::debug("Key {}", key.value());
+            delKeys.push_back(key.value());
+        }
+        redis->del(delKeys.begin(), delKeys.end());
+
+        return true;
+    }
+}
