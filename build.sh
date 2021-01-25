785e77a5cee5960cc7f48dd882cefb2a3135ba53
jlmathews <jlmathews8@gmail.com>
2021-01-24 17:15:34 -0800

    Cleanup cmake scripts and build scripts. Add run tests option (#10)

--- build.sh
+++ build.sh
@@ -1,4 +1,25 @@
 
+EnableTests=OFF
+GenerateDocuments=OFF
+
+while getopts td flag
+do
+    case "${flag}" in
+        t)
+        EnableTests=ON
+        echo "Tests Enabled"
+        ;;
+        d)
+        GenerateDocuments=ON
+        echo "Document Generation Enabled"
+        ;;
+        \?)
+        echo "Invalid Option: -$OPTARG" 1>&2
+        exit 1
+        ;;
+    esac
+done
+
 LUAINC=/usr/include/lua5.3/
 LUALIB="/usr/lib/x86_64-linux-gnu;/usr/lib/x86_64-linux-gnu/liblua5.3.so"
 
@@ -7,13 +28,22 @@ cd build/
 
 # cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=lib/hiredis/build -DCMAKE_INSTALL_PREFIX=build/lib/ -DREDIS_PLUS_PLUS_BUILD_SHARED=OFF -DREDIS_PLUS_PLUS_BUILD_TEST=OFF -DREDIS_PLUS_PLUS_CXX_STANDARD=17 ..
 # cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=lib/hiredis -DCMAKE_INCLUDE_PATH=lib -DREDIS_PLUS_PLUS_BUILD_TEST=OFF ..
-cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_DOC=OFF -DLUA_INCLUDE_DIR=$LUAINC -DLUA_LIBRARIES=$LUALIB ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_DOC=$GenerateDocuments -DBUILD_TESTS=$EnableTests -DLUA_INCLUDE_DIR=$LUAINC -DLUA_LIBRARIES=$LUALIB ..
 
-if ! make all; then
-	printf 'Make failed\n'
-	exit 2
+echo $EnableTests
+
+if [ $EnableTests = "ON" ]
+then
+    if ! make test; then
+        printf 'Make failed\n'
+        exit 2
+    fi
+else
+    if ! make all; then
+        printf 'Make failed\n'
+        exit 2
+    fi
 fi
 
 echo 'Done'
 cd ..
-
